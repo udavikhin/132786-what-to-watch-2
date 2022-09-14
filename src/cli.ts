84aa610fb9ceb8ec40a41bfc3b5c05702d8e4231
Anatoly Udavikhin <anatoly.udavikhin@gmail.com>
2022-09-14 12:43:46 +0300

    Добавляет shebang для cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import VersionCommand from './cli-command/version-command.js';
 import HelpCommand from './cli-command/help-command.js';
 import CLIApplication from './app/cli-application.js';
