aba6d0d09c76b211be4034cf212c8060fcbf2e8b
Anatoly Udavikhin <anatoly.udavikhin@gmail.com>
2022-09-19 16:50:04 +0300

    Добавляет тип MockData

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,12 @@
+export type MockData = {
+  titles: string[],
+  descriptions: string[],
+  previewVideoSrcs: string[],
+  videoSrcs: string[],
+  actors: string[],
+  directors: string[],
+  users: string[],
+  posterSrcs: string[],
+  backgroundSrcs: string[],
+  bgColors: string[]
+}
